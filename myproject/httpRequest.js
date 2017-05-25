38944f162055a7c2ffc97df3d5d78587cd8d628a
David Lee <karisma70kr@gmail.com>
2017-05-25 22:23:30 +0900

    소스 일부 수정

--- myproject/httpRequest.js
+++ myproject/httpRequest.js
@@ -4,6 +4,7 @@
 var httpRequest = (function(){
     var xmlhttp = null;
 
+
     function createRequest() {
         var http = null;
 
@@ -15,31 +16,17 @@ var httpRequest = (function(){
         return http;
     }
 
-    function receiveEvent( propertyFunction, method, callback ) {
-        if (xmlhttp.readyState != 4 ){
-            if( propertyFunction == "onload") {
-                alert("Error!! readyState Code : " + xmlhttp.readyState);
-            } else {
-                console.log("readyState Code : " + xmlhttp.readyState);
-            }
-            return;
-        }
-
-        if( xmlhttp.status == 200) {
-            if (method == "HEAD") {
-                alert(xmlhttp.getAllResponseHeaders());
-                callback( xmlhttp );
-            } else {
-                xmlhttp.dataLength = xmlhttp.getResponseHeader("Content-Length");
-                xmlhttp.dataType = xmlhttp.getResponseHeader("Content-Type");
-
-                callback( xmlhttp );
+    function getStateString( stateNumber ){
+        for( prop in xmlhttp ) {
+            if ( typeof(xmlhttp[prop]) == "number") {
+                if (xmlhttp[prop] == stateNumber) {
+                    return prop;
+                }
             }
         }
-        else{
-            alert( "Error!! status code : " + xmlhttp.status );
-        }
+        return null;
     }
+
 
     function requestData( method, url, callback ){
         if( xmlhttp == null ){
@@ -47,14 +34,53 @@ var httpRequest = (function(){
         }
 
         var urlArray = null;
-
+/*
         if( typeof( xmlhttp.onload ) != "undefined"){
-            xmlhttp.onload = function() {
-                receiveEvent( "onload", method, callback );
+            xmlhttp.onload = function( e ) {
+                // receiveEvent( "onload", method, callback );
+                console.log( "readyState : " + getStateString( e.target.readyState ) + ", status : " + e.target.status );
+                callback( xmlhttp );
             }
         } else {
-            xmlhttp.onreadystatechange = function () {
-                receiveEvent( "onreadystatechange", method, callback );
+            xmlhttp.onreadystatechange = function ( e ) {
+                console.log( "readyState : " + getStateString( e.target.readyState ) + ", status : " + e.target.status );
+                if( e.target.readyState == XMLHttpRequest.DONE ) {
+                    if (e.target.status == 200) {
+                        callback(xmlhttp);
+                    } else {
+                        alert( "Error!!!  code : " + e.target.status );
+                    }
+                }
+            }
+        }
+*/
+        xmlhttp.onreadystatechange = function ( e ) {
+         //    console.log( "readyState : " + getStateString( e.target.readyState ) + ", status : " + e.target.status );
+            //fdsfsdfsd
+            if( e.target.readyState == XMLHttpRequest.DONE ) {
+                if (e.target.status == 200) {
+                    callback(xmlhttp);
+                } else {
+                    alert( "Error!!!  code : " + e.target.status );
+                }
+            }
+        };
+
+        if( typeof( xmlhttp.abort ) != "undefined"){
+            xmlhttp.abort = function(){
+                console.log( "abort!!!");
+            }
+        }
+
+        if( typeof( xmlhttp.error ) != "undefined"){
+            xmlhttp.error = function(){
+                abort( "error !!!");
+            }
+        }
+
+        if( typeof( xmlhttp.timeout ) != "undefined"){
+            xmlhttp.error = function(){
+                console.log( "timeout !!!");
             }
         }
 
@@ -62,7 +88,10 @@ var httpRequest = (function(){
             urlArray = url.split("?");
             xmlhttp.open(method, urlArray[0], true);
         } else {
-            xmlhttp.open(method, url, true);
+            if( method == "HEAD")
+                xmlhttp.open(method, url, false );
+            else
+                xmlhttp.open(method, url, true );
         }
 
         // if ( xmlhttp.overrideMimeType)       // 사용자 정의 파일 다운로드시
@@ -77,11 +106,17 @@ var httpRequest = (function(){
     }
 
     return function( method, url, callback ){
+        // requestData( method, url, callback );
+
         requestData( "HEAD", url, function (http) {
+            // alert(xmlhttp.getAllResponseHeaders());
+            http.dataLength = parseInt( http.getResponseHeader("Content-Length"));
+            http.dataType = http.getResponseHeader("Content-Type");
             http.responseAttachText = "Flie Len : " + parseInt( http.getResponseHeader("Content-Length"), 10)
                 + ", Last Updated Date : " + http.getResponseHeader("Last-Modified")
                 + ", Content type : " + http.getResponseHeader("Content-Type");
             requestData( method, url, callback );
         });
+
     }
 }());
