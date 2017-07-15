4eeefc93176834c64fe79df2b792a0adeea3f342
David Lee <karisma70kr@gmail.com>
2017-07-15 11:51:11 +0900

    함수의 할당에서 ; 누락 부분 모두 추가

--- biblemap/src/binarywrapper.js
+++ biblemap/src/binarywrapper.js
@@ -14,47 +14,47 @@ function BinaryFileWrapper(binFile) {
         var byte = binFile.getByteAt(this.position);
         this.position++;
         return byte;
-    }
+    };
 
     this.getLength = function() {
         return binFile.getLength();
-    }
+    };
 
     this.getSByte = function() {
         var sbyte = binFile.getSByteAt(this.position);
         this.position++;
         return sbyte;
-    }
+    };
 
     this.getShort = function() {
         var short = binFile.getShortAt(this.position, this.bigEndian);
         this.position += 2;
         return short;
-    }
+    };
     
     this.getSShort = function() {
         var sshort = binFile.getSShortAt(this.position, this.bigEndian);
         this.position += 2;
         return sshort;
-    }
+    };
     
     this.getLong = function() {
         var l = binFile.getLongAt(this.position, this.bigEndian);
         this.position += 4;
         return l;
-    }
+    };
     
     this.getSLong = function() {
         var l = binFile.getSLongAt(this.position, this.bigEndian);
         this.position += 4;
         return l;
-    }
+    };
     
     this.getString = function(iLength) {
         var s = binFile.getStringAt(this.position, iLength);
         this.position += iLength;
         return s;
-    }
+    };
 
 	this.getDoubleAt = function(iOffset, bBigEndian) {
 		// hugs stackoverflow
@@ -66,17 +66,17 @@ function BinaryFileWrapper(binFile) {
 		var s = a >> 31 ? -1 : 1;
 		var e = (a >> 52 - 32 & 0x7ff) - 1023;
 		return s * (a & 0xfffff | 0x100000) * 1.0 / Math.pow(2,52-32) * Math.pow(2, e) + b * 1.0 / Math.pow(2, 52) * Math.pow(2, e);
-	}
+	};
 
     this.getDouble = function() {    
         var d = this.getDoubleAt(this.position, this.bigEndian);
         this.position += 8;
         return d;
-    }
+    };
 
     this.getChar = function() {
         var c = binFile.getCharAt(this.position);
         this.position++;
         return c;
-    }
+    };
 }
